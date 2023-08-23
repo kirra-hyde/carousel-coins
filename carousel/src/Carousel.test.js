d9538ba69aeb87b724c4fece43a671846dd7c3aa
Daniel Chou <daniel.cw.chou@gmail.com>
2023-08-22 20:33:08 -0400

    WIP Part 4

--- carousel/src/Carousel.test.js
+++ carousel/src/Carousel.test.js
@@ -30,6 +30,38 @@ it("works when you click on the right arrow", function() {
   ).toBeInTheDocument();
 });
 
+it("works when you click on the left arrow", function() {
+  const { container } = render(
+    <Carousel
+      photos={TEST_IMAGES}
+      title="images for testing"
+    />
+  );
+  // expect the first image to show, but not the second
+  expect(
+    container.querySelector('img[alt="testing image 1"]')
+  ).toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="testing image 2"]')
+  ).not.toBeInTheDocument();
+
+  // move forward in the carousel
+  const rightArrow = container.querySelector(".bi-arrow-right-circle");
+  fireEvent.click(rightArrow);
+
+  // move backward in the carousel
+  const leftArrow = container.querySelector(".bi-arrow-left-circle");
+  fireEvent.click(leftArrow);
+
+  // expect the first image to show, but not the third
+  expect(
+    container.querySelector('img[alt="testing image 3"]')
+  ).not.toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="testing image 1"]')
+  ).toBeInTheDocument();
+});
+
 it("doesn't crash", function() {
   render(<Carousel photos={TEST_IMAGES} title="images for testing"/>);
 });
@@ -40,3 +72,53 @@ it("matches snapshot", function() {
 
   expect(container).toMatchSnapshot();
 });
+
+
+it("left arrow doesn't display at first image", function(){
+  const { container, debug } = render(
+    <Carousel
+      photos={TEST_IMAGES}
+      title="images for testing"
+    />
+  );
+
+  expect(
+    container.querySelector('img[alt="testing image 1"]')
+  ).toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="testing image 2"]')
+  ).not.toBeInTheDocument();
+
+  const leftArrow = container.querySelector(".bi-arrow-left-circle");
+  debug(leftArrow);
+  expect(
+    leftArrow
+  ).not.toBeInTheDocument();
+})
+
+it("right arrow doesn't display at third image", function(){
+  const { container, debug } = render(
+    <Carousel
+      photos={TEST_IMAGES}
+      title="images for testing"
+    />
+  );
+
+  // move forward twice in the carousel
+  const rightArrow = container.querySelector(".bi-arrow-right-circle");
+  fireEvent.click(rightArrow);
+  fireEvent.click(rightArrow);
+
+  expect(
+    container.querySelector('img[alt="testing image 3"]')
+  ).toBeInTheDocument();
+
+  debug(rightArrow);
+  expect(
+    rightArrow
+  ).not.toBeInTheDocument();
+})
+
+
+
+
